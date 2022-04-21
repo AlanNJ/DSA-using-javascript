8ff39a53eb38536e81e21f4521cfd1a02b2709ca
AlanNJ <alannpn75@gmail.com>
2022-04-21 16:25:53 +0530

    added-solution-code

--- time and space complexity/CaeSarCipher Algorithm/caesarCipher.js
+++ time and space complexity/CaeSarCipher Algorithm/caesarCipher.js
@@ -1,1 +1,36 @@
-// Caesar Cipher
+// Caesar Cipher Algorithm
+
+function caesarCipher(string, step) {
+	let letters = "abcdefghijklmnopqrstuvwxyz";
+	let result = "";
+	for (let i = 0; i <= string.length - 1; i++) {
+		let idx = letters.indexOf(string[i]);
+		let new_letter = letters[(idx + step) % 26];
+		result += new_letter;
+	}
+	console.log(result);
+}
+
+// It works for special symbols,characters as well.
+
+function caesarCipher(string, step) {
+	let letters = "abcdefghijklmnopqrstuvwxyz";
+	let result = "";
+	for (let i = 0; i <= string.length - 1; i++) {
+		let idx = letters.indexOf(string[i]);
+		if (idx == -1) {
+			result += string[i];
+			continue;
+		}
+		let new_letter = letters[(idx + step) % 26];
+		result += new_letter;
+	}
+	console.log(result);
+}
+caesarCipher("alan!", 10);
+
+/*
+time complexity: O(n)
+space complexity : O(2) = O(1) (We are creating a new string to store our result)
+
+*/
