477edefc908213e21886519ac414328d551ecec4
AlanNJ <alannpn75@gmail.com>
2022-04-26 18:59:04 +0530

    first-commit

---
+++ selectionSort/index.js
@@ -0,0 +1,17 @@
+// Problem: Sort the given array using selection sort.
+var selectionSort = function (arr) {
+	for (i = 0; i <= arr.length - 1; i++) {
+		let swapIdx = i;
+		for (j = i + 1; j <= arr.length - 1; j++) {
+			if (arr[j] < arr[i]) {
+				swapIdx = j;
+				let temp = arr[swapIdx];
+				arr[swapIdx] = arr[i];
+				arr[i] = temp;
+			}
+		}
+	}
+	console.log(arr);
+};
+let arr = [5, 4, 3, 2, 1];
+selectionSort(arr);
