97a220348599cc6fc785442477604bf5a3b3e946
AlanNJ <alannpn75@gmail.com>
2022-04-28 23:19:03 +0530

    added-code-explaination

--- insertionSort/index.js
+++ insertionSort/index.js
@@ -2,14 +2,23 @@
 
 function insertionSort(inputArr) {
 	for (i = 1; i < inputArr.length; i++) {
-		let j = i - 1;
-		let current = inputArr[i];
+		let j = i - 1; // triggering the element before current element in loop
+		let current = inputArr[i]; // setting our second element to check elements befor it
 		while (j > -1 && current < inputArr[j]) {
-			inputArr[j + 1] = inputArr[j];
+			//runs for every element befor the current
+			inputArr[j + 1] = inputArr[j]; // replaces small element to its position
 			j--;
 		}
-		inputArr[j + 1] = current;
+		inputArr[j + 1] = current; // set the sorted element to its original position
 	}
 	return inputArr;
 }
 console.log(insertionSort([2, 3, 6, 4, 8]));
+
+/*
+
+time complexity: O(n^2) worst case
+space complexity : O(1)
+
+Note: Though it has quadratic time complexity,it outperforms even bubble and merge sort when the number of input is less like 20,30
+*/
